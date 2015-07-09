4984440442059564eabca01b15c7bf4024317443
RHays91 <rhaysvm42@gmail.com>
2015-07-08 20:02:51 -0700

    Eliminates redundant lineup event handler

--- src/init.js
+++ src/init.js
@@ -2,20 +2,17 @@ $(document).ready(function(){
   window.dancers = [];
   var dancerFactory = new DancerFactory();
 
-
-  var lineUpDancers = function(event){
+  var lineUpDancers = function(){
     var centerHeight = $("body").height()/2;
     var centerWidth = $("body").width()/(dancers.length+1);
     for (var i = 0; i < dancers.length; i++) {
-
       dancers[i].setPosition(centerHeight,centerWidth*(i+1));
-
     }
   };
 
   $(".addCatButton").on("click", function(event){
 
-    // make a dancer with a random position
+    // make a catDancer with a random position
     var dancer = dancerFactory.createDancer(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
@@ -25,13 +22,9 @@ $(document).ready(function(){
     $('body').append(dancer.$node);
   });
 
-  $(".lineupDancerButton").on("click", function(event){
-    lineUpDancers();
-  });
-
   $(".addDancerButton").on("click", function(event){
 
-    // make a dancer with a random position
+    // make a blinkyDancer with a random position
     var dancer = dancerFactory.createDancer(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
@@ -41,5 +34,8 @@ $(document).ready(function(){
     $('body').append(dancer.$node);
   });
 
-  $(".lineUpDancersButton").on("click", lineUpDancers);
+  $(".lineupDancerButton").on("click", function(event){
+    lineUpDancers();
+  });
+
 });
